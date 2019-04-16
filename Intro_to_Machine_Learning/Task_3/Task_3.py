a08b998a8f555ffe015665360b1768b82f72661f
Parker <parker.ewen5441@gmail.com>
2019-04-16 20:16:12 +0200

    Changed NN architecture, still need to add k-fold cross-validation and skip connections

--- Intro_to_Machine_Learning/Task_3/Task_3.py
+++ Intro_to_Machine_Learning/Task_3/Task_3.py
@@ -46,9 +46,10 @@ class Data:
 
 
 class NeuralNet:
-    def __init__(self, train, k_fold=10):
+    def __init__(self, train, epochs, k_fold=10):
         self.train = train
         self.k_fold = k_fold
+        self.epochs = epochs
         self.model = []
 
         self.trainnet()
@@ -58,21 +59,17 @@ class NeuralNet:
         self.model = keras.Sequential([
             keras.layers.Dense(1024, activation='relu'),
             keras.layers.Dropout(0.2),
-            keras.layers.Dense(1024, activation='relu'),
-            keras.layers.Dropout(0.2),
-            keras.layers.Dense(512, activation='relu'),
-            keras.layers.Dropout(0.2),
             keras.layers.Dense(512, activation='relu'),
             keras.layers.Dropout(0.2),
             keras.layers.Dense(256, activation='relu'),
-            keras.layers.Dropout(0.2),
-            keras.layers.Dense(256, activation='relu'),
+            keras.layers.Dense(128, activation='relu'),
             keras.layers.Dropout(0.2),
             keras.layers.Dense(5, activation='softmax')])
         self.model.compile(optimizer='adam',
                            loss='sparse_categorical_crossentropy',
                            metrics=['accuracy'])
-        self.model.fit(self.train.features, self.train.classes, epochs=50)
+        self.model.fit(self.train.features, self.train.classes,
+                       epochs=self.epochs)
 
 
 def Output(predictions, Id):
@@ -89,7 +86,7 @@ def main():
     train = Data("train.h5", True)
     test = Data("test.h5", False)
 
-    neuralnet = NeuralNet(train, 10)
+    neuralnet = NeuralNet(train, 100)
     y_pred = np.argmax(neuralnet.model.predict(test.features), axis=1)
 
     start = train.features.shape[0]
